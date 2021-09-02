0c27f157dbf9b457cc404074907c815aa4c0a706
Konstantin Klimov <59954139+kklimov-axcient@users.noreply.github.com>
2021-09-02 18:25:00 +0300

    CL-2228 Update CS and SZS for PROD (#354)

--- axcient/szs/post-install.mk
+++ axcient/szs/post-install.mk
@@ -13,11 +13,13 @@ post-install:
 	${MV} ${STAGEDIR}${PREFIX}/bin/safetyarchivecleanup ${STAGEDIR}${PREFIX}/bin/szs-op-safety-archive-cleanup
 	${MV} ${STAGEDIR}${PREFIX}/bin/cleanupclones ${STAGEDIR}${PREFIX}/bin/szs-op-cleanup-clones
 	${MV} ${STAGEDIR}${PREFIX}/bin/statistics ${STAGEDIR}${PREFIX}/bin/szs-op-statistics
+	${MV} ${STAGEDIR}${PREFIX}/bin/snapshotscleanup ${STAGEDIR}${PREFIX}/bin/szs-op-snapshots-cleanup
 	${INSTALL_LIB} ${WRKDIR}/lib/libszs-op.so ${STAGEDIR}${PREFIX}/lib
 	${INSTALL_DATA} ${WRKDIR}/lib/libszs-op.h ${STAGEDIR}${PREFIX}/include/szs-op.h
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/cron.d/safety_archive_cleanup ${STAGEDIR}${EXAMPLESDIR}/cron.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/cron.d/cleanup_clones ${STAGEDIR}${EXAMPLESDIR}/cron.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/cron.d/statistics ${STAGEDIR}${EXAMPLESDIR}/cron.d/
+	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/cron.d/snapshots_cleanup ${STAGEDIR}${EXAMPLESDIR}/cron.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/newsyslog.conf.d/szs.conf ${STAGEDIR}${EXAMPLESDIR}/newsyslog.conf.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/newsyslog.conf.d/szs_exports.conf ${STAGEDIR}${EXAMPLESDIR}/newsyslog.conf.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/newsyslog.conf.d/szs_jobs.conf ${STAGEDIR}${EXAMPLESDIR}/newsyslog.conf.d/
