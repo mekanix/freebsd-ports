2224ff6dd63297fad939ba2c4325fa14a4690655
Alan Somers <asomers@axcient.com>
2021-10-04 20:32:09 +0000

    sysutils/py-salt: fix the cron.present patch
    I generated this patch incorrectly for the last PR.

--- sysutils/py-salt/files/patch-salt_states_cron.py
+++ sysutils/py-salt/files/patch-salt_states_cron.py
@@ -1,4 +1,4 @@
---- salt/states/cron.py.orig	2021-10-04 17:49:08 UTC
+--- salt/states/cron.py.orig	2021-10-04 20:29:18 UTC
 +++ salt/states/cron.py
 @@ -360,6 +360,9 @@ def present(
          return ret
@@ -18,27 +18,37 @@
          data = __salt__["cron.set_special"](
              user=user,
              special=special,
-@@ -381,14 +385,21 @@ def present(
+@@ -381,20 +385,27 @@ def present(
              commented=commented,
              identifier=identifier,
          )
 -    if data == "present":
+-        ret["comment"] = "Cron {0} already present".format(name)
 +    if data == "present" and antidata == "removed":
 +        ret["comment"] = "Duplicate Cron {} removed".format(name)
-+        return ret
-+
-+    if data == "present" and antidata == "absent":
-         ret["comment"] = "Cron {} already present".format(name)
          return ret
  
 -    if data == "new":
+-        ret["comment"] = "Cron {0} added to {1}'s crontab".format(name, user)
++    if data == "present" and antidata == "absent":
++        ret["comment"] = "Cron {} already present".format(name)
++        return ret
++
 +    if data == "new" and antidata == "absent":
-         ret["comment"] = "Cron {} added to {}'s crontab".format(name, user)
++        ret["comment"] = "Cron {} added to {}'s crontab".format(name, user)
          ret["changes"] = {user: name}
          return ret
-+
+ 
 +    if data == "new" and antidata == "removed":
 +        ret["comment"] = "Cron {} time specification changed".format(name)
- 
++
      if data == "updated":
-         ret["comment"] = "Cron {} updated".format(name)
+-        ret["comment"] = "Cron {0} updated".format(name)
++        ret["comment"] = "Cron {} updated".format(name)
+         ret["changes"] = {user: name}
+         return ret
+-    ret["comment"] = "Cron {0} for user {1} failed to commit with error \n{2}".format(
++    ret["comment"] = "Cron {} for user {} failed to commit with error \n{}".format(
+         name, user, data
+     )
+     ret["result"] = False
