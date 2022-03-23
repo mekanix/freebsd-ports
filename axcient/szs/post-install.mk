13effa9cd0143497d3cdedf38bd6df1de751e873
Konstantin Klimov <59954139+kklimov-axcient@users.noreply.github.com>
2022-03-23 12:05:27 +0300

    CL-3950 Update szs and dralserver prod ports (#460)

--- axcient/szs/post-install.mk
+++ axcient/szs/post-install.mk
@@ -14,12 +14,14 @@ post-install:
 	${MV} ${STAGEDIR}${PREFIX}/bin/cleanupclones ${STAGEDIR}${PREFIX}/bin/szs-op-cleanup-clones
 	${MV} ${STAGEDIR}${PREFIX}/bin/statistics ${STAGEDIR}${PREFIX}/bin/szs-op-statistics
 	${MV} ${STAGEDIR}${PREFIX}/bin/snapshotscleanup ${STAGEDIR}${PREFIX}/bin/szs-op-snapshots-cleanup
+	${MV} ${STAGEDIR}${PREFIX}/bin/clonesnapshots ${STAGEDIR}${PREFIX}/bin/szs-op-clone-snapshots
 	${INSTALL_LIB} ${WRKDIR}/lib/libszs-op.so ${STAGEDIR}${PREFIX}/lib
 	${INSTALL_DATA} ${WRKDIR}/lib/libszs-op.h ${STAGEDIR}${PREFIX}/include/szs-op.h
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/cron.d/safety_archive_cleanup ${STAGEDIR}${EXAMPLESDIR}/cron.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/cron.d/cleanup_clones ${STAGEDIR}${EXAMPLESDIR}/cron.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/cron.d/statistics ${STAGEDIR}${EXAMPLESDIR}/cron.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/cron.d/snapshots_cleanup ${STAGEDIR}${EXAMPLESDIR}/cron.d/
+	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/cron.d/clone_snapshots ${STAGEDIR}${EXAMPLESDIR}/cron.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/newsyslog.conf.d/szs.conf ${STAGEDIR}${EXAMPLESDIR}/newsyslog.conf.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/newsyslog.conf.d/szs_exports.conf ${STAGEDIR}${EXAMPLESDIR}/newsyslog.conf.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/newsyslog.conf.d/szs_jobs.conf ${STAGEDIR}${EXAMPLESDIR}/newsyslog.conf.d/
@@ -31,6 +33,7 @@ post-install:
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/rsyslog.d/cleanup_clones.conf ${STAGEDIR}${EXAMPLESDIR}/rsyslog.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/rsyslog.d/safety_archive_cleanup.conf ${STAGEDIR}${EXAMPLESDIR}/rsyslog.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/rsyslog.d/snapshots_cleanup.conf ${STAGEDIR}${EXAMPLESDIR}/rsyslog.d/
+	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/rsyslog.d/clone_snapshots.conf ${STAGEDIR}${EXAMPLESDIR}/rsyslog.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/rsyslog.d/statistics.conf ${STAGEDIR}${EXAMPLESDIR}/rsyslog.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/rsyslog.d/szs_exports.conf ${STAGEDIR}${EXAMPLESDIR}/rsyslog.d/
 	${INSTALL_DATA} ${WRKSRC}/freebsd-pkg/usr/local/etc/szs/config.yml.example ${STAGEDIR}${PREFIX}/etc/szs/config.yml.sample
