b717e3227aa6e9ad6b714d62a663c497ddac7cbe
Alan Somers <asomers@axcient.com>
2022-05-31 20:17:37 +0000

    devel/py-libzfs: fix scrub/resilver percentage and time remaining
    The percentage calculation was using bytes issued per pass instead of total
    bytes issued, and as a result it was wrong after a reboot.
    
    The total_secs_left calculation was using the bytes examined, not the bytes
    issued, so it was underestimating the amount of work remaining, and could
    even go negative.

--- devel/py-libzfs/files/patch-libzfs.pyx
+++ devel/py-libzfs/files/patch-libzfs.pyx
@@ -1,4 +1,4 @@
---- libzfs.pyx.orig	2021-10-01 16:36:11 UTC
+--- libzfs.pyx.orig	2022-02-02 17:12:49 UTC
 +++ libzfs.pyx
 @@ -29,7 +29,18 @@ include "config.pxi"
  include "nvpair.pxi"
@@ -19,7 +19,39 @@
  class DatasetType(enum.IntEnum):
      FILESYSTEM = zfs.ZFS_TYPE_FILESYSTEM
      VOLUME = zfs.ZFS_TYPE_VOLUME
-@@ -3766,8 +3777,10 @@ cdef class ZFSDataset(ZFSResource):
+@@ -2369,23 +2380,28 @@ cdef class ZPoolScrub(object):
+     property bytes_scanned:
+         def __get__(self):
+             if self.stats != NULL:
+-                return self.stats.pss_issued
++                return self.stats.pss_examined
+ 
+     property total_secs_left:
+         def __get__(self):
+             if self.state != ScanState.SCANNING:
+                 return
+ 
+-            examined = self.bytes_scanned
+             total = self.bytes_to_scan
++            issued = self.bytes_issued
+             elapsed = ((int(time.time()) - self.stats.pss_pass_start) - self.stats.pss_pass_scrub_spent_paused) or 1
+             pass_issued = self.stats.pss_pass_issued or 1
+             issue_rate = pass_issued / elapsed
+-            return int((total - examined) / issue_rate)
++            return int((total - issued) / issue_rate)
+ 
+     property bytes_issued:
+         def __get__(self):
+             if self.stats != NULL:
++                return self.stats.pss_issued
++
++    property bytes_issued_per_pass:
++        def __get__(self):
++            if self.stats != NULL:
+                 return self.stats.pss_pass_issued
+ 
+     property pause:
+@@ -3883,8 +3899,10 @@ cdef class ZFSDataset(ZFSResource):
          if flags:
              convert_sendflags(flags, &cflags)
  
@@ -31,7 +63,7 @@
  
          if err != 0:
              raise self.root.get_error()
-@@ -4020,6 +4033,7 @@ cdef class ZFSSnapshot(ZFSResource):
+@@ -4139,6 +4157,7 @@ cdef class ZFSSnapshot(ZFSResource):
              return result
  
      def get_send_progress(self, fd):
@@ -39,7 +71,7 @@
          IF HAVE_ZFS_IOCTL_HEADER:
              cdef zfs.zfs_cmd_t cmd
              memset(&cmd, 0, cython.sizeof(zfs.zfs_cmd_t))
-@@ -4027,7 +4041,10 @@ cdef class ZFSSnapshot(ZFSResource):
+@@ -4146,7 +4165,10 @@ cdef class ZFSSnapshot(ZFSResource):
              cdef int ret
  
              cmd.zc_cookie = fd
