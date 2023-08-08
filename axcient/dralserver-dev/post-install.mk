bfc77a486f2a46980ef6ab1d6be5e5a87a8601f7
aleksandaraxc <108737041+aleksandaraxc@users.noreply.github.com>
2023-08-08 17:17:10 +0200

    CL-6432: dral-dev new binary for ftps exports cleanup (#799)

--- axcient/dralserver-dev/post-install.mk
+++ axcient/dralserver-dev/post-install.mk
@@ -7,10 +7,14 @@ post-install:
 	${MKDIR} ${STAGEDIR}${EXAMPLESDIR}/newsyslog.conf.d
 	${MKDIR} ${STAGEDIR}${EXAMPLESDIR}/rsyslog.d
 	${MV} ${STAGEDIR}${PREFIX}/bin/cleanupdralclones ${STAGEDIR}${PREFIX}/bin/dralserver-cleanup-clones
+	${MV} ${STAGEDIR}${PREFIX}/bin/cleanupdralftpsexports ${STAGEDIR}${PREFIX}/bin/dralserver-cleanup-ftps-exports
 	${INSTALL_DATA} ${WRKSRC}/build/freebsd-pkg/usr/local/etc/cron.d/dralserver-cleanup-clones ${STAGEDIR}${EXAMPLESDIR}/cron.d/
+	${INSTALL_DATA} ${WRKSRC}/build/freebsd-pkg/usr/local/etc/cron.d/dralserver-cleanup-ftps-exports ${STAGEDIR}${EXAMPLESDIR}/cron.d/
 	${INSTALL_DATA} ${WRKSRC}/build/freebsd-pkg/usr/local/etc/newsyslog.conf.d/dralserver.conf ${STAGEDIR}${EXAMPLESDIR}/newsyslog.conf.d
 	${INSTALL_DATA} ${WRKSRC}/build/freebsd-pkg/usr/local/etc/newsyslog.conf.d/dralserver-cleanup-clones.conf ${STAGEDIR}${EXAMPLESDIR}/newsyslog.conf.d
+	${INSTALL_DATA} ${WRKSRC}/build/freebsd-pkg/usr/local/etc/newsyslog.conf.d/dralserver-cleanup-ftps-exports.conf ${STAGEDIR}${EXAMPLESDIR}/newsyslog.conf.d
 	${INSTALL_SCRIPT} ${WRKSRC}/build/freebsd-pkg/usr/local/etc/rc.d/dralserver ${STAGEDIR}${PREFIX}/etc/rc.d
 	${INSTALL_DATA} ${WRKSRC}/build/freebsd-pkg/usr/local/etc/rsyslog.d/dralserver.conf ${STAGEDIR}${EXAMPLESDIR}/rsyslog.d
 	${INSTALL_DATA} ${WRKSRC}/build/freebsd-pkg/usr/local/etc/rsyslog.d/dralserver-cleanup-clones.conf ${STAGEDIR}${EXAMPLESDIR}/rsyslog.d
+	${INSTALL_DATA} ${WRKSRC}/build/freebsd-pkg/usr/local/etc/rsyslog.d/dralserver-cleanup-ftps-exports.conf ${STAGEDIR}${EXAMPLESDIR}/rsyslog.d
 	${INSTALL_DATA} ${WRKSRC}/build/freebsd-pkg/usr/local/etc/dralserver/dralserver.yml.example  ${STAGEDIR}${ETCDIR}/dralserver.yml.sample
