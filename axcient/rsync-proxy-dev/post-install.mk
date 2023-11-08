4423632bb40b17731178dbc1d454f271dd0638df
Konstantin Klimov <59954139+kklimov-axcient@users.noreply.github.com>
2023-11-08 17:31:12 +0400

    CL-7499 Fix path after refactoring in rsync-proxy (#848)
    **Changes**
    
    * Just change post-install.mk path for rsync-proxy port

--- axcient/rsync-proxy-dev/post-install.mk
+++ axcient/rsync-proxy-dev/post-install.mk
@@ -5,7 +5,7 @@ post-install:
 	${MKDIR} ${STAGEDIR}${EXAMPLESDIR}/newsyslog.conf.d
 	${MKDIR} ${STAGEDIR}${EXAMPLESDIR}/rsyslog.d
 	${MV} ${STAGEDIR}${PREFIX}/bin/proxy ${STAGEDIR}${PREFIX}/bin/rsync-proxy
-	${INSTALL_DATA} ${WRKSRC}/build/freebsd-pkg/usr/local/etc/newsyslog.conf.d/rsync-proxy.conf ${STAGEDIR}${EXAMPLESDIR}/newsyslog.conf.d
-	${INSTALL_SCRIPT} ${WRKSRC}/build/freebsd-pkg/usr/local/etc/rc.d/rsync_proxy ${STAGEDIR}${PREFIX}/etc/rc.d
-	${INSTALL_DATA} ${WRKSRC}/build/freebsd-pkg/usr/local/etc/rsyslog.d/rsync-proxy.conf ${STAGEDIR}${EXAMPLESDIR}/rsyslog.d
-	${INSTALL_DATA} ${WRKSRC}/build/freebsd-pkg/usr/local/etc/rsync-proxy/rsync-proxy.yml.example  ${STAGEDIR}${ETCDIR}/rsync-proxy.yml.sample
+	${INSTALL_DATA} ${WRKSRC}/build/freebsd/usr/local/etc/newsyslog.conf.d/rsync-proxy.conf ${STAGEDIR}${EXAMPLESDIR}/newsyslog.conf.d
+	${INSTALL_SCRIPT} ${WRKSRC}/build/freebsd/usr/local/etc/rc.d/rsync_proxy ${STAGEDIR}${PREFIX}/etc/rc.d
+	${INSTALL_DATA} ${WRKSRC}/build/freebsd/usr/local/etc/rsyslog.d/rsync-proxy.conf ${STAGEDIR}${EXAMPLESDIR}/rsyslog.d
+	${INSTALL_DATA} ${WRKSRC}/build/freebsd/usr/local/etc/rsync-proxy/rsync-proxy.yml.example  ${STAGEDIR}${ETCDIR}/rsync-proxy.yml.sample
